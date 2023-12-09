a56a5053d59a09be41882fb8e27b88b50984080b
student <student@BTU.local>
2023-12-09 12:24:16 +0400

    first commit

---
+++ src/components/Contact/index.js
@@ -0,0 +1,31 @@
+const Contact = ({
+  contactData,
+  checkedIds,
+  onToggleContactFromList,
+  handleDelete,
+}) => {
+  return (
+    <tr>
+      <td>
+        <input
+          type="checkbox"
+          onChange={(e) => onToggleContactFromList(e, contactData.id)}
+          checked={checkedIds.includes(contactData.id)}
+        />
+      </td>
+      <td>
+        <i
+          className="fa fa-trash-o"
+          onClick={() => handleDelete(contactData.id)}
+        />
+      </td>
+      <td>
+        <i className="fa fa-pencil"></i>
+      </td>
+      <td>{contactData.name}</td>
+      <td>{contactData.phoneNumber}</td>
+    </tr>
+  );
+};
+
+export default Contact;
